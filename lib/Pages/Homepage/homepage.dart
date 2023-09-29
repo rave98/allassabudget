acf97becd13784e9f22ad54f9e479b112781f666
Elia Ravella <eliaravella@gmail.com>
2023-09-29 16:44:52 +0200

    feat: added camera capabilities to homepage

--- lib/Pages/Homepage/homepage.dart
+++ lib/Pages/Homepage/homepage.dart
@@ -1,9 +1,32 @@
+import 'package:allassabudget/logger.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:pie_chart/pie_chart.dart';
+import 'package:image_picker/image_picker.dart';
 
 /// HomePage of the application, displaying basic information
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  late ImagePicker _imagePicker;
+
+  HomePage({super.key}) {
+    _imagePicker = ImagePicker();
+  }
+
+  void pickImage() async {
+    logger.info("Opening camera");
+    XFile? newImage = await _imagePicker.pickImage(source: ImageSource.camera);
+
+    if(newImage == null) {
+      return; // TODO: add snackbar / toast message for user, 'Failed retrieving image'
+    }
+    
+    logger.info("Saving image");
+    newImage.saveTo(
+      // path + filename
+      (await getApplicationDocumentsDirectory()).path + DateTime.now().millisecondsSinceEpoch.toString()
+    );
+    
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +34,7 @@ class HomePage extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         MaterialButton(
-          onPressed: () {}, 
+          onPressed: pickImage, 
           child: Icon(Icons.add_circle, color: Theme.of(context).primaryColor, size: 300,),),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
