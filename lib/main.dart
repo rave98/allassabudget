231d33a61f9f0f8dcc1a18792994f9ef366d919a
Elia Ravella <eliaravella@gmail.com>
2023-12-22 12:07:12 +0100

    fix: homepage is now default page opened

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:allassabudget/Storage/storage.dart';
 import 'package:allassabudget/Pages/AddExpenseManually/add_expense_manually.dart';
 import 'package:allassabudget/Pages/BudgetCap/budget_cap.dart';
 import 'package:allassabudget/Pages/Categories/categories.dart';
-import 'package:allassabudget/Pages/RecurringExpenses/recurring_expenses.dart';
 import 'package:allassabudget/Pages/Homepage/homepage.dart';
 import 'package:allassabudget/app_theme.dart';
 import 'package:allassabudget/logger.dart';
@@ -43,10 +42,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 2;
+  int _currentIndex = 1;
 
   final List<Widget> _pages = [
-    RecurringExpenses(),
     AddExpenseManually(),
     HomePage(),
     BudgetCap(),
@@ -68,7 +66,6 @@ class _MyHomePageState extends State<MyHomePage> {
         selectedItemColor: Theme.of(context).primaryColor,
         unselectedItemColor: Theme.of(context).unselectedWidgetColor,
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.restart_alt), label: 'Recurring expenses'),
           BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add expense'),
           BottomNavigationBarItem(icon: Icon(Icons.timeline), label: ''),
           BottomNavigationBarItem(icon: Icon(Icons.stop_circle), label: 'Budget cap'),
