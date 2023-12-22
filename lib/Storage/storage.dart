16983164b4ecfd1e4dc241d11f93a27b0ea31ad0
Elia Ravella <eliaravella@gmail.com>
2023-12-22 12:07:40 +0100

    feat: added helpre storage functions

--- lib/Storage/storage.dart
+++ lib/Storage/storage.dart
@@ -27,7 +27,9 @@ class Storage {
   /// performs a select sum(price) from expenses group by category
   /// returns a map that associates each category to the total expenses for that category
   static Future<Map<String, double>> categoryGroupedExpenses() async {
-    List<Expense> expenses  = await localStorage!.expenses.where().findAll();
+    List<Expense> expenses  = await localStorage!.expenses
+      .where()
+      .findAll();
     Map<String, double> groupedExpenses = {};
     for (var e in expenses) {
       for (var c in e.categories) {
@@ -35,5 +37,23 @@ class Storage {
       }
     }
     return groupedExpenses;
+  }
+
+  /// selects all the egresses from the wallet and returns them aggregated
+  static Future<int> losses() async {
+    return localStorage!.expenses
+      .filter()
+      .amountGreaterThan(0)
+      .amountProperty()
+      .sum();
+  }
+
+  /// selects all gains in the wallet and returns them aggregated
+  static Future<int> gains() async {
+    return localStorage!.expenses
+      .filter()
+      .amountLessThan(0)
+      .amountProperty()
+      .sum();
   }
 }
