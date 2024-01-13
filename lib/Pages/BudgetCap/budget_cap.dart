f51d648444b40796c3f9fd55cccd668d08ce83cb
Elia Ravella <eliaravella@gmail.com>
2024-01-13 12:34:21 +0100

    feat: outlining budget cap page

--- lib/Pages/BudgetCap/budget_cap.dart
+++ lib/Pages/BudgetCap/budget_cap.dart
@@ -1,10 +1,22 @@
+import 'package:allassabudget/Pages/AddExpenseManually/categories_grid_selectable.dart';
+import 'package:allassabudget/Pages/AddExpenseManually/categories_selecting_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class BudgetCap extends StatelessWidget {
   BudgetCap({super.key});
+  final CategoriesSelectingController categoriesController = CategoriesSelectingController();
+
+  void confirmCapCategories() {
+    
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("BudgetCap page!"),);
+    return Column(
+      children: [
+        CategoriesGridSelectable(categoriesController),
+        ElevatedButton(onPressed: confirmCapCategories, child: Text(AppLocalizations.of(context)!.addExpense))
+      ]);
   }
 }
