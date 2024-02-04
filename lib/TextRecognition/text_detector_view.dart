33e8fc14dcd97e9a269130b7ec047421dcb3b06c
Elia Ravella <eliaravella@gmail.com>
2024-02-04 17:48:51 +0100

    feat: WIP recognizing price from camera

--- lib/TextRecognition/text_detector_view.dart
+++ lib/TextRecognition/text_detector_view.dart
@@ -1,3 +1,4 @@
+import 'package:allassabudget/logger.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
@@ -31,64 +32,27 @@ class _TextRecognizerViewState extends State<TextRecognizerView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        DetectorView(
-          title: 'Text Detector',
-          customPaint: _customPaint,
-          text: _text,
-          onImage: _processImage,
-          initialCameraLensDirection: _cameraLensDirection,
-          onCameraLensDirectionChanged: (value) => _cameraLensDirection = value,
-        ),
-        Positioned(
-            top: 30,
-            left: 100,
-            right: 100,
-            child: Row(
-              children: [
-                Spacer(),
-                Container(
-                    decoration: BoxDecoration(
-                      color: Colors.black54,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: _buildDropdown(),
-                    )),
-                Spacer(),
-              ],
-            )),
+      body: Column(
+        children: [
+          Expanded(
+            child: DetectorView(
+              title: 'Text Detector',
+              customPaint: _customPaint,
+              text: _text,
+              onImage: _processImage,
+              initialCameraLensDirection: _cameraLensDirection,
+              onCameraLensDirectionChanged: (value) => _cameraLensDirection = value,
+            )
+          ),
+          Row(
+            children: [
+              Text("Select price"),
+              Text(_text ?? "no text recognized")
+            ],
+          )
       ]),
     );
   }
-
-  Widget _buildDropdown() => DropdownButton<TextRecognitionScript>(
-        value: _script,
-        icon: const Icon(Icons.arrow_downward),
-        elevation: 16,
-        style: const TextStyle(color: Colors.blue),
-        underline: Container(
-          height: 2,
-          color: Colors.blue,
-        ),
-        onChanged: (TextRecognitionScript? script) {
-          if (script != null) {
-            setState(() {
-              _script = script;
-              _textRecognizer.close();
-              _textRecognizer = TextRecognizer(script: _script);
-            });
-          }
-        },
-        items: TextRecognitionScript.values
-            .map<DropdownMenuItem<TextRecognitionScript>>((script) {
-          return DropdownMenuItem<TextRecognitionScript>(
-            value: script,
-            child: Text(script.name),
-          );
-        }).toList(),
-      );
 
   Future<void> _processImage(InputImage inputImage) async {
     if (!_canProcess) return;
@@ -100,13 +64,8 @@ class _TextRecognizerViewState extends State<TextRecognizerView> {
     final recognizedText = await _textRecognizer.processImage(inputImage);
     if (inputImage.metadata?.size != null &&
         inputImage.metadata?.rotation != null) {
-      final painter = TextRecognizerPainter(
-        recognizedText,
-        inputImage.metadata!.size,
-        inputImage.metadata!.rotation,
-        _cameraLensDirection,
-      );
-      _customPaint = CustomPaint(painter: painter);
+      setState(() => _text = recognizedText.text);
+      logger.info("text: ${recognizedText.text}");
     } else {
       _text = 'Recognized text:\n\n${recognizedText.text}';
       // TODO: set _customPaint to draw boundingRect on top of image
